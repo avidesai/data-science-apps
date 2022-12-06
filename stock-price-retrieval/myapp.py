f4e3a018283a333b243a0609d78ea2eab5134b91
Avi Desai <avidesai2@gmail.com>
2022-12-06 02:49:14 -0800

    Updated app and fixed requirements

--- stock-price-retrieval/myapp.py
+++ stock-price-retrieval/myapp.py
@@ -5,11 +5,16 @@ import pandas as pd
 st.write("""
 # Simple Stock Price App
 
-Shown are the **closing prices** and **volume** of Google stock
+Shown are the **closing prices** and **volume** of the entered stock
 
 """)
 
+
 tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+
+st.sidebar.header("Stock Selection")
+tickerSymbol = st.sidebar.text_input("Enter your ticker below")
 tickerData = yf.Ticker(tickerSymbol)
 
 tickerDf = tickerData.history(period = '1d', start = '2004-8-19', end = '2022-8-19')
